5b8175b3dab5cee7fe1ed1542b84fb5d67bc0c10
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-01-27 23:24:19 +0900

    feat: default値の追加

--- outputs.js
+++ outputs.js
@@ -7,7 +7,7 @@ const {
 const { dummuy } = require('./dummy-outputs.js')
 
 const exportColumns = [
-  { name: '伝票区切', default: '0' },
+  { name: '伝票区切', default: '1' },
   { name: '連番', default: '0' },
   { name: '未使用' },
   { name: '伝票日付', from: '月日' },
@@ -22,10 +22,11 @@ const exportColumns = [
   {
     name: '借方内訳ｺｰﾄﾞ',
     from: '借方補助',
+    default: '000000',
     convert: getSmileCodeIfInputed
   },
   { name: '借方税ｺｰﾄﾞ' },
-  { name: '借方部門ｺｰﾄﾞ' },
+  { name: '借方部門ｺｰﾄﾞ', default: '000000' },
   { name: '未使用' },
   { name: '未使用' },
   { name: '未使用' },
@@ -62,7 +63,7 @@ const exportColumns = [
     convert: getSmileCodeIfInputed
   },
   { name: '貸方税ｺｰﾄﾞ' },
-  { name: '貸方部門ｺｰﾄﾞ' },
+  { name: '貸方部門ｺｰﾄﾞ', default: "000000" },
   { name: '未使用' },
   { name: '未使用' },
   { name: '未使用' },
